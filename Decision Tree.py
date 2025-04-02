5966b42c789805e9aec1bc2b6bf5318a29193d94
Akindipe Muheez <akindipemuheez@gmail.com>
2025-04-02 11:13:33 +0100

    Descision Tree fix

--- Decision Tree.py
+++ Decision Tree.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 import pandas as pd
+from sklearn.datasets import load_iris
 
 iris = load_iris()
 
