4a2a2aecdaedc9f3506cd681e808570dee0cfa3f
Akindipe Muheez <akindipemuheez@gmail.com>
2025-04-03 09:23:16 +0100

    Hierarchical Clustering

---
+++ Heirarchical Clustering.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import make_blobs
+from scipy.cluster.hierarchy import dendrogram, linkage
+import matplotlib.pyplot as plt
+
+X, _ = make_blobs(n_samples=300, centers=4, random_state=42)
+
+linkage_matrix = linkage(X, method='average')
+
+plt.figure(figsize=(12,8))
+dendrogram(linkage_matrix)
+
+plt.title('Hierarchical Dendrogram')
+plt.xlabel('data point')
+plt.ylabel('Distance')
+plt.show()
